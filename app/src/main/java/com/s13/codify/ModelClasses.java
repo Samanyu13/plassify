13f761b61314c87335c5f6003ab89f02a9ba6858
Samanyu13 <samanyusaji18@gmail.com>
2021-01-03 20:26:53 +0530

    first commit

---
+++ app/src/main/java/com/s13/codify/ModelClasses.java
@@ -0,0 +1,7 @@
+package com.s13.codify;
+
+public class ModelClasses {
+    public static String[] MODEL_CLASSES = new String[]{
+            "Cars", "Memes", "Mountains", "Selfies", "Trees", "Whatsapp_Screenshots"
+    };
+}
